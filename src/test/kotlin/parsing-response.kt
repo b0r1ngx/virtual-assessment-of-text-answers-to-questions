a0437dcab6a8c1d297b38b80fff4b6095b2071ba
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-02-28 12:53:19 +0300

    refactoring to test

--- src/test/kotlin/parsing-response.kt
+++ src/test/kotlin/parsing-response.kt
@@ -3,22 +3,19 @@ import java.io.File
 import kotlin.test.assertEquals
 
 internal fun parsingResponseTest() {
-    val responses = listOf(
+    val rawJsonToAnswers = listOf(
         "prompt-response-200.json" to 10,
         "prompt-response-at-web-to-documentation-style.json" to 8,
     )
-    for (response in responses) {
+    for (rawJsonToAnswer in rawJsonToAnswers) {
         var promptResponse = ""
-        val rawJsonList = File(
-            "./src/main/resources/${response.first}"
-        ).readLines(charset = Charsets.UTF_8)
-        for (line in rawJsonList)
-            promptResponse += line.trim()
+        File("./src/main/resources/${rawJsonToAnswer.first}")
+            .readLines(charset = Charsets.UTF_8)
+            .forEach { promptResponse += it.trim() }
 
-        val estimation = getEstimationFromPromptResponse(promptResponse)
         assertEquals(
-            actual = response.second,
-            expected = estimation
+            actual = rawJsonToAnswer.second,
+            expected = getEstimationFromPromptResponse(promptResponse)
         )
     }
 }
