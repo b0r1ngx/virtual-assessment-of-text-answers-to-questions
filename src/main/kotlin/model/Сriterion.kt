6c89a8349cea05d94201c5ade399ddf2d248f94c
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-08 13:37:11 +0300

    1) Add one more criterion - Completeness. 1) Add TODOs

--- src/main/kotlin/model/Сriterion.kt
+++ src/main/kotlin/model/Сriterion.kt
@@ -1,10 +1,20 @@
 package dev.boringx.model
 
+/* TODO: Рассмотреть возможность добавления дополнительных критериев?
+ *  1. Степень осознанности, понимания изученного, (это можно понять из суммы используемых критериев)
+ *  2. Грамотность, (мало важен)
+ *  3. Языковое оформление ответа (мало важен)
+ *
+ *  TODO: Choose criteria dynamically?
+ *   1. What field of question?
+ *   2. Analyze answer? (how long it is, etc..)
+ */
 enum class Criterion(
     val text: String
 ) {
     Correctness("правильности"),
     Brevity("краткости"),
     Style("стиля"),
+    Completeness("полнота"),
     Coherence("связности ответа"),
 }
