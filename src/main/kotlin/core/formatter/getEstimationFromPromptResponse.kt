0430bb0b5f447b057c1470b11ddc4e5b8b6a5a51
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-05-02 21:10:28 +0300

    Remove try/catch block, because now function is execution-safety (because each of operation can't throw an exception)

--- src/main/kotlin/core/formatter/getEstimationFromPromptResponse.kt
+++ src/main/kotlin/core/formatter/getEstimationFromPromptResponse.kt
@@ -8,17 +8,12 @@ private val INT_PATTERN = Regex("\\d+")
 // TODO: Помимо оценки, также можно сохранять и выдавать полный ответ (комментарий) БЯМ на ответ.
 //        (полезно, для ответа на вопрос: почему была поставлена такая оценка?)
 fun getAssessmentFromPromptResponse(response: String): Int {
-    try {
-        val deserializing =
-            response.decodeFromStringSafety<PromptResponse.Success>()
+    val deserializing =
+        response.decodeFromStringSafety<PromptResponse.Success>()
 
-        val promptResponse = deserializing
-            ?.result?.alternatives?.firstOrNull()?.message?.text ?: ""
+    val promptResponse = deserializing
+        ?.result?.alternatives?.firstOrNull()?.message?.text ?: ""
 
-        val assessment = INT_PATTERN.find(promptResponse)
-        return assessment?.value?.toIntOrNull() ?: -1
-    } catch (e: Exception) {
-        println(e.stackTrace)
-        return -1
-    }
+    val assessment = INT_PATTERN.find(promptResponse)
+    return assessment?.value?.toIntOrNull() ?: -1
 }
