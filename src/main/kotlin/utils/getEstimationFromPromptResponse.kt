5d2955470f12d991cbc52a323ace1e20f7b5e0a8
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-05-02 20:49:35 +0300

    Created TODO

--- src/main/kotlin/utils/getEstimationFromPromptResponse.kt
+++ src/main/kotlin/utils/getEstimationFromPromptResponse.kt
@@ -4,6 +4,8 @@ import dev.boringx.model.prompt.response.PromptResponse
 
 private val INT_PATTERN = Regex("\\d+")
 
+// TODO: Помимо оценки, также можно сохранять и выдавать полный ответ (комментарий) БЯМ на ответ.
+//        (полезно, для ответа на вопрос: почему была поставлена такая оценка?)
 fun getAssessmentFromPromptResponse(response: String): Int {
     try {
         val deserializing =
