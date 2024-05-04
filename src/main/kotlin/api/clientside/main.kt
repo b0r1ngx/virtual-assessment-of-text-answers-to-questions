38a68600bca42d87a676e4e66845c6096c30f5f9
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-05 00:10:50 +0300

    1) Extract routing via module() 2) Explicit declare host for embeddedServer()

--- src/main/kotlin/api/clientside/main.kt
+++ src/main/kotlin/api/clientside/main.kt
@@ -1,5 +1,6 @@
 package dev.boringx.api.clientside
 
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -8,11 +9,14 @@ import io.ktor.server.routing.routing
 import io.ktor.server.routing.get
 
 fun main() {
-    embeddedServer(factory = Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
+    embeddedServer(factory = Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
         }
-    }.start(wait = true)
+    }
 }
