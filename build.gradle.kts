d4708d765691e3e6c689a6a699ad0f6dd308b55c
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-13 19:16:00 +0300

    added sqldelight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,23 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
+    id("app.cash.sqldelight") version "2.0.2"
 }
 
 group = "dev.boringx"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
 }
 
 // TODO: After some point, try to use guide explained here https://github.com/ktorio/ktor-documentation/tree/2.3.10/codeSnippets/snippets/embedded-server-native
 //  to run a Ktor server in a Kotlin/Native application (not via JVM ?)
 dependencies {
+    // don't understand to the end, what sqldelight implementation to use here...
+    // below dependency (native) on macOS indicates error
+//    implementation("app.cash.sqldelight:native-driver:2.0.2")
     implementation("io.ktor:ktor-server-core:2.3.10")
     implementation("io.ktor:ktor-server-netty:2.3.10")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
@@ -28,4 +33,12 @@ tasks.test {
 
 kotlin {
     jvmToolchain(20)
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set(group.toString())
+        }
+    }
 }
