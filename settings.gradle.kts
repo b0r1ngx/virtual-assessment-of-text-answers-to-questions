d6fedbf05f37db60b483b9ac85e2cc7c9f56532d
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-14 15:04:03 +0300

    1) declare pluginManagement & dependencyResolutionManagement with repositories
    2) declare created modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,35 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+//plugins {
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+//}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
 }
 
 rootProject.name = "virtual-assessment-of-text-answers-to-questions"
+include("common")
+include("backend")
+include("compose")
