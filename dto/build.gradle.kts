78adcc5664a794879da01a4e12fa9cb17aaba898
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-16 16:07:00 +0300

    created gradle file for dto module

---
+++ dto/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlinx.datetime)
+                api(libs.kotlinx.serialization)
+            }
+        }
+    }
+}
