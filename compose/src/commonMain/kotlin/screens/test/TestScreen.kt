e723b96a36d9c2a1a85b326443eeeb14c5ec22a7
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-16 21:57:17 +0300

    start implement Test Screen

---
+++ compose/src/commonMain/kotlin/screens/test/TestScreen.kt
@@ -0,0 +1,22 @@
+package screens.test
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestScreen() {
+    Column {
+        // progressBar?
+        // Text(text= "currentQuestion / questions.size"
+
+        // question.text
+
+        // TextField to input answer
+
+        // if currentQuestion == questions.last
+        //      Finish Button
+        // else
+        //      Button for navigate to next question
+
+    }
+}
