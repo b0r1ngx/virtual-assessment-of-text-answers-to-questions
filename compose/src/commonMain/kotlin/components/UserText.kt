75838e6d13ca4032da56ff92faeab231d30530ea
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-18 20:22:24 +0300

    Implement UserText to show a little information about User as Text

---
+++ compose/src/commonMain/kotlin/components/UserText.kt
@@ -0,0 +1,26 @@
+package components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import users.User
+
+@Composable
+fun UserText(
+    user: User,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.clickable {
+
+    }) {
+        Icon(imageVector = Icons.Default.Person, contentDescription = null)
+        Text(
+            text = user.name,
+        )
+    }
+}
