2de9dca8bc1414b780aa1b1a6e9aa95da0cb785b
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-18 22:40:48 +0300

    resolve TODOs, tested on iOS (thanks to Multiplatform available features of kotlinx.datetime <3)

--- compose/src/commonMain/kotlin/screens/utils/DateTimeFormatter.kt
+++ compose/src/commonMain/kotlin/screens/utils/DateTimeFormatter.kt
@@ -4,10 +4,8 @@ import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
-// TODO: to use it, get user TimeZone
-// TODO: delete default timezone?
-fun Instant.toHumanReadable(timeZone: TimeZone = TimeZone.UTC): String {
-    with(toLocalDateTime(timeZone)) {
+fun Instant.toHumanReadable(): String {
+    with(toLocalDateTime(TimeZone.currentSystemDefault())) {
         return "$hour:$minute, $dayOfMonth.$monthNumber.$year"
     }
 }
