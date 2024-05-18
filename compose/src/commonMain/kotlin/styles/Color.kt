72ea09e8cdb3cc50f45bc4f67f6310766885e153
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-18 22:29:39 +0300

    add colors to colorized tests at Passed Tab of TestsScreen

--- compose/src/commonMain/kotlin/styles/Color.kt
+++ compose/src/commonMain/kotlin/styles/Color.kt
@@ -1,2 +1,7 @@
 package styles
 
+import androidx.compose.ui.graphics.Color
+
+val GreenPastelColor = Color(0xFF_BFFCC6)
+val YellowPastelColor = Color(0xFF_FFF5BA)
+val RedPastelColor = Color(0xFF_FFBEBC) // FFCBC1
