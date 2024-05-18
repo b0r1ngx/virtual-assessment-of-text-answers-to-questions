b71987466b05335c8d9ad81559a6d181081cabdd
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-18 17:18:30 +0300

    Continue implement things on TestsScreen

--- compose/src/commonMain/kotlin/screens/tests/TestsScreen.kt
+++ compose/src/commonMain/kotlin/screens/tests/TestsScreen.kt
@@ -2,83 +2,144 @@ package screens.tests
 
 import Test
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import components.UserText
 import dev.boringx.compose.generated.resources.Res
 import dev.boringx.compose.generated.resources.end_at
+import dev.boringx.compose.generated.resources.no_available_tests
 import dev.boringx.compose.generated.resources.questions
 import dev.boringx.compose.generated.resources.start_at
+import dev.boringx.compose.generated.resources.tests
+import kotlinx.datetime.Clock
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.stringResource
 import screens.tests.tabs.TestsTab
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun TestsScreen(testsViewModel: TestsViewModel) {
-    val selectedTab = remember { mutableStateOf(TestsTab.Available) }
+    val selectedTab by remember { mutableStateOf(TestsTab.Available) }
 
     Column {
-        Text(
-            text = "TestsScreen",
-            style = MaterialTheme.typography.h2,
+        Title(
+            text = stringResource(Res.string.tests),
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = MaterialTheme.colors.background),
-        ) // title, style
+                .background(color = MaterialTheme.colors.surface)
+        )
 
-        CustomTabs(selectedTab = selectedTab)
+        CustomTabs(
+            selectedTab = selectedTab,
+            modifier = Modifier.fillMaxWidth()
+        )
 
-//        if (testsViewModel.uiState.value.isLoading) {
-//            return
-//        }
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            if (testsViewModel.tests.isEmpty()) {
-                // show empty container
-                Text(text = "No available tests") // show it in center
-            } else {
-                // show tests, divided by course
-                LazyColumn {
-                    // divide by test.course
-                    items(items = testsViewModel.tests) {
-                        TestCard(test = it)
-                    }
-                }
-            }
-        }
+        Tests(
+            selectedTab = selectedTab,
+            tests = testsViewModel.tests,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.background.copy(alpha = .5f))
+        )
     }
+}
+
+// TODO: Extract it later, to use on other Screens
+@Composable
+private fun Title(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.h4,
+    )
 }
 
 @Composable
 private fun CustomTabs(
-    selectedTab: MutableState<TestsTab>,
+    selectedTab: TestsTab,
     modifier: Modifier = Modifier
 ) {
-    Row {
-        TestsTab.entries.forEach {
+    val tabs = remember { TestsTab.entries.toTypedArray() }
+    var selectedIndex by remember(selectedTab) {
+        mutableIntStateOf(tabs.indexOf(selectedTab))
+    }
+    TabRow(
+        selectedTabIndex = selectedIndex,
+        modifier = modifier,
+        backgroundColor = Color.Transparent,
+    ) {
+        tabs.forEachIndexed { index, tab ->
             Tab(
-                selected = it == selectedTab.value,
-                onClick = { selectedTab.value = it }
+                selected = selectedIndex == index,
+                onClick = { selectedIndex = index },
+                text = { Text(text = tab.name) },
             )
         }
     }
 }
 
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun Tests(
+    selectedTab: TestsTab,
+    tests: List<Test>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (tests.isEmpty()) {
+            Text(text = stringResource(Res.string.no_available_tests))
+        } else {
+            LazyColumn(verticalArrangement = Arrangement.SpaceBetween) {
+                // divide by test.course
+                items(items = tests.filter {
+                    when (selectedTab) {
+                        TestsTab.Available -> it.end_at > Clock.System.now()
+                        TestsTab.Passed -> {
+                            // TODO: search in studentTests
+                            it.end_at < Clock.System.now()
+                        }
+
+                        TestsTab.Missed -> {
+                            // gone
+                            it.end_at < Clock.System.now()
+                            // and not passed
+                        }
+                    }
+
+                }) {
+                    TestCard(test = it)
+                }
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -86,25 +147,18 @@ private fun TestCard(
     test: Test,
     modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier) {
-        Column {
+    Card(
+        modifier = modifier
+            .padding(bottom = 10.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(modifier = Modifier.padding(5.dp)) {
             Text(text = test.name)
             UserText(user = test.creator)
-            Row {
-                Row(modifier = Modifier.weight(1f)) {
-                    Text(text = stringResource(Res.string.start_at))
-                    Text(text = test.start_at.toString())
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Row(modifier = Modifier.weight(1f)) {
-                    Text(text = stringResource(Res.string.end_at))
-                    Text(text = test.end_at.toString()) // use .format to provide DateTimeFormat
-                }
-            }
-            Row {
-                Text(text = test.questions.size.toString())
-                Text(text = stringResource(Res.string.questions))
-            }
+            // TimeText: use .format to provide DateTimeFormat
+            Text(text = stringResource(Res.string.start_at) + "${test.start_at}")
+            Text(text = stringResource(Res.string.end_at) + "${test.end_at}")
+            Text(text = "${test.questions.size}" + stringResource(Res.string.questions))
         }
     }
 }
