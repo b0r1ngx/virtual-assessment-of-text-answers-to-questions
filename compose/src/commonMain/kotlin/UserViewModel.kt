13ec455168263e64cc2979e7cee7293bf4d540b3
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 00:35:15 +0300

    Thinks that it may be helpful to have UserViewModel

---
+++ compose/src/commonMain/kotlin/UserViewModel.kt
@@ -0,0 +1,8 @@
+import model.UserType
+
+class UserViewModel {
+    val userType: UserType = UserType.Student
+//        get() {
+//            //// get from shared pref, if null, navigate user to Auth Screen
+//        }
+}
