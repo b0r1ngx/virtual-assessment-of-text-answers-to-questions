cf396f51b4a86eda874012983e22cd924431f5fe
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 04:51:31 +0300

    declare module right where start embeddedServer

--- backend/src/main/kotlin/api/server/main.kt
+++ backend/src/main/kotlin/api/server/main.kt
@@ -1,6 +1,9 @@
 package api.server
 
-import api.server.routes.module
+import Repository
+import SqlDriverFactory
+import api.server.routes.configureRouting
+import createDatabase
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -12,4 +15,9 @@ fun main() {
         host = "0.0.0.0",
         module = Application::module
     ).start(wait = true)
+}
+
+fun Application.module() {
+    val repository = Repository(createDatabase(SqlDriverFactory()))
+    configureRouting(repository)
 }
--- backend/src/main/kotlin/api/server/routes/configureRouting.kt
+++ backend/src/main/kotlin/api/server/routes/configureRouting.kt
@@ -1,19 +1,12 @@
 package api.server.routes
 
 import Repository
-import SqlDriverFactory
-import createDatabase
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
-import io.ktor.server.routing.routing
 import io.ktor.server.routing.get
-
-fun Application.module() {
-    val repository = Repository(createDatabase(SqlDriverFactory()))
-    configureRouting(repository)
-}
+import io.ktor.server.routing.routing
 
 fun Application.configureRouting(repository: Repository) {
     routing {
