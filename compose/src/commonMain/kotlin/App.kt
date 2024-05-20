7a4a4b556dccd5f442437abf395105f111b36bb0
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 03:34:19 +0300

    at composable App, init repository and viewModels for screens

--- compose/src/commonMain/kotlin/App.kt
+++ compose/src/commonMain/kotlin/App.kt
@@ -1,41 +1,26 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import client.Repository
+import client.network.ClientApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import screens.auth.AuthScreen
+import screens.auth.AuthViewModel
+import screens.tests.TestsScreen
+import screens.tests.TestsViewModel
 
-import dev.boringx.compose.generated.resources.Res
-import dev.boringx.compose.generated.resources.compose_multiplatform
-
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
-fun App() {
+fun App(sqlDriverFactory: SqlDriverFactory) {
+    val repository = Repository(
+        database = createDatabase(sqlDriverFactory),
+        api = ClientApi()
+    )
+    val userViewModel = UserViewModel(repository)
+    val authViewModel = AuthViewModel(repository)
+    val testsViewModel = TestsViewModel(repository)
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text(greeting)
-                }
-            }
-        }
+        AuthScreen(userViewModel, authViewModel)
+//        TestsScreen(userViewModel, testsViewModel)
     }
 }
