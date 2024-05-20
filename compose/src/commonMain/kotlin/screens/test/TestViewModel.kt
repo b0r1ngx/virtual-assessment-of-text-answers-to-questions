0ade79f24a99841d47ec388679c9de21a473cab5
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-21 00:56:55 +0300

    holding testName when creating test in viewModel

--- compose/src/commonMain/kotlin/screens/test/TestViewModel.kt
+++ compose/src/commonMain/kotlin/screens/test/TestViewModel.kt
@@ -1,8 +1,13 @@
 package screens.test
 
 import Test
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class TestViewModel(val test: Test) {
+    var testName by mutableStateOf("")
+
     fun saveTest() {
         TODO("Not yet implemented")
     }
