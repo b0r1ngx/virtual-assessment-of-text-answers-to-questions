9467c7bdf4d32e8ebdbb8d855dddec4ca062e89e
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 04:14:40 +0300

    1) extract routing to separate function 2) using repository

--- backend/src/main/kotlin/api/server/routes/main.kt
+++ backend/src/main/kotlin/api/server/routes/main.kt
@@ -1,15 +1,29 @@
 package api.server.routes
 
+import Repository
+import SqlDriverFactory
+import createDatabase
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.routing
 import io.ktor.server.routing.get
 
 fun Application.module() {
+    val repository = Repository(createDatabase(SqlDriverFactory()))
+    configureRouting(repository)
+}
+
+fun Application.configureRouting(repository: Repository) {
     routing {
         get("/") {
             call.respondText("Hello, world!")
+        }
+
+        get("/tests") {
+            val tests = repository.getTests()
+            call.respond(tests)
         }
     }
 }
