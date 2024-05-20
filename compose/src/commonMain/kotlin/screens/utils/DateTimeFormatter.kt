07984ab96dcda7878b911a36463fd45f34725c8a
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 16:30:46 +0300

    add one more extension function - to fast translate Int to Duration by Hour period

--- compose/src/commonMain/kotlin/screens/utils/DateTimeFormatter.kt
+++ compose/src/commonMain/kotlin/screens/utils/DateTimeFormatter.kt
@@ -3,6 +3,8 @@ package screens.utils
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 fun Instant.toHumanReadable(): String {
     with(toLocalDateTime()) {
@@ -12,3 +14,5 @@ fun Instant.toHumanReadable(): String {
 
 fun Instant.toLocalDateTime() =
     toLocalDateTime(TimeZone.currentSystemDefault())
+
+fun Int.toHours() = this.toDuration(DurationUnit.HOURS)
