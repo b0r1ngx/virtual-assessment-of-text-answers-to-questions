a1b0953e85db2f6fad5d879d01a1afeb04d4798f
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-20 14:44:40 +0300

    provide default user: Teacher, for tests

--- compose/src/commonMain/kotlin/UserViewModel.kt
+++ compose/src/commonMain/kotlin/UserViewModel.kt
@@ -1,10 +1,11 @@
 import client.Repository
+import model.UserType
 import users.User
 
 class UserViewModel(
     private val repository: Repository
 ) {
-    var user: User? = null
+    var user: User? = User(type = UserType.Teacher.ordinal, name = "", email = "")
 //        get() {
 //            //// get from shared pref, if null, navigate user to Auth Screen
 //        }
