e1fd144f26fb56b287f267a890fb3497c4340703
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-22 23:55:09 +0300

    1) Pre define createTest 2) Rename client Repository to ClientRepository

--- common/src/commonMain/kotlin/Repository.kt
+++ common/src/commonMain/kotlin/Repository.kt
@@ -1,5 +1,6 @@
 import dev.boringx.Database
 import dev.boringx.Test
+import dev.boringx.TestQuestions
 import users.User
 
 open class Repository(private val database: Database) {
@@ -10,7 +11,46 @@ open class Repository(private val database: Database) {
     }
 
     open suspend fun getTests(): List<Test> {
-        return database.testQueries.selectAll().executeAsList()
+        // todo: use join in one query, instead tons of queries
+        val tests = database.testQueries.selectAll().executeAsList()
+        val testQuestions = mutableListOf<TestQuestions>()
+        tests.forEach { test ->
+//            testQuestions.add(
+//                database.testQuestionsQueries
+//                    .selectBy(test_id = test.id)
+//                    .executeAsList()
+//            )
+        }
+        val questions = tests.forEach {
+
+        }
+        return tests
+    }
+
+    open suspend fun createTest(test: Test) {
+        return database.testQueries.insert(
+            creator_id = test.creator_id,
+            course_id = test.course_id,
+            name = test.name,
+            start_at = test.start_at,
+            end_at = test.end_at,
+            created_at = test.created_at
+        )
+    }
+
+    open suspend fun addAnswers(
+        student: User,
+        test: Test,
+        answers: Map<Question, Answer>
+    ) {
+        answers.forEach { (question, answer) ->
+//            database.answerQueries.insert(
+//                test_id = test.id,
+//                question_id = question.id,
+//                student_id = student.id,
+//                text = answer.text
+//            )
+        }
     }
 
     open suspend fun createUser(user: User) {
--- common/src/commonMain/kotlin/client/ClientRepository.kt
+++ common/src/commonMain/kotlin/client/ClientRepository.kt
@@ -1,27 +1,33 @@
 package client
 
+import Repository
 import client.network.ClientApi
 import dev.boringx.Database
 import dev.boringx.Test
-import Repository
 import users.User
 
-class Repository(
+class ClientRepository(
     private val database: Database,
     private val api: ClientApi,
 ) : Repository(database = database) {
 
+    override suspend fun createTest(test: Test) {
+        // map modelTest to Test DB Entity
+        super.createTest(test)
+        api.createTest(test)
+    }
+
     override suspend fun getTests(): List<Test> {
         val localTests = super.getTests()
         val remoteTests = api.getTests()
-        remoteTests.forEach {
+        remoteTests.forEach { test ->
             database.testQueries.insert(
-                creator_id = it.creator_id,
-                course_id = it.course_id,
-                name = it.name,
-                start_at = it.start_at,
-                end_at = it.end_at,
-                created_at = it.created_at
+                creator_id = test.creator_id,
+                course_id = test.course_id,
+                name = test.name,
+                start_at = test.start_at,
+                end_at = test.end_at,
+                created_at = test.created_at
             )
         }
         return remoteTests.ifEmpty { localTests }
