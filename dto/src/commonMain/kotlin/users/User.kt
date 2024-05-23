f8d02f954cb56a9df6f88a773d7988e8df43c698
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-23 13:11:44 +0300

    add courses parameter

--- dto/src/commonMain/kotlin/users/User.kt
+++ dto/src/commonMain/kotlin/users/User.kt
@@ -1,10 +1,14 @@
 package users
 
+import Course
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
     val type: Int, // UserType
     val name: String,
-    val email: String
+    val email: String,
+    // TODO: later, delete default value? because in real app workflow, it is not possible to user has zero courses,
+    //  but for tests its may be allowed (when user pick 0 courses on AuthScreen)
+    val courses: List<Course> = listOf()
 )
