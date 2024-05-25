a163e96545d751bd66572f22e54e523024cb109b
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-25 20:03:48 +0300

    continue implement logic at PassingTestScreen & PassingTestViewModel

--- compose/src/commonMain/kotlin/screens/test/PassingTestScreen.kt
+++ compose/src/commonMain/kotlin/screens/test/PassingTestScreen.kt
@@ -1,31 +1,74 @@
 package screens.test
 
+import Answer
+import Question
+import User
+import UserViewModel
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import components.TopBar
 import dev.boringx.compose.generated.resources.Res
 import dev.boringx.compose.generated.resources.complete_test
+import dev.boringx.compose.generated.resources.save_button
+import dev.boringx.compose.generated.resources.test
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun PassingTestScreen(
+    userViewModel: UserViewModel,
     testViewModel: PassingTestViewModel
 ) {
-    // info about test with questions
+    Column {
+        TopBar(
+            title = stringResource(Res.string.test),
+            subtitle = stringResource(TestScreenStatus.Passing.res),
+            onBackButtonClick = testViewModel.onFinished,
+        )
 
-    // if testViewModel.testInProgress draw one things, else draw info about test
-    // EditingTest.info, like at card
-    LazyColumn {
-//        items(testViewModel.test.questions) {
-//
-//        }
+        // info about test with questions
+
+        // if testViewModel.testInProgress draw one things, else draw info about test
+        // EditingTest.info, like at card
+        Column {
+            // progressBar?
+            LazyColumn {
+                items(testViewModel.questionsToAnswers.value) {
+                    QuestionWithAnswerField(
+                        user = userViewModel.user,
+                        questionToAnswer = it,
+                        saveAnswers = testViewModel::saveAnswers
+                    )
+                }
+            }
+        }
+
+
+        Button(onClick = {
+            testViewModel.saveAnswers(
+                user = userViewModel.user,
+                answers = testViewModel.questionsToAnswers.value,
+                isTestCompleted = true
+            )
+        }) {
+            Text(stringResource(Res.string.complete_test))
+        }
     }
-    Button(onClick = { }) {
-        Text(stringResource(Res.string.complete_test))
-    }
-    // progressBar?
-    // Text(text= "currentQuestion / questions.size"
 
     // question.text
 
@@ -36,4 +79,39 @@ fun PassingTestScreen(
     // else
     //      Button for navigate to next question
     //        (disable this button, if TextField.value.isEmpty)
+}
+
+@Composable
+private fun QuestionWithAnswerField(
+    user: User,
+    questionToAnswer: Pair<Question, Answer>,
+    saveAnswers: (user: User, answer: List<Pair<Question, Answer>>, isTestCompleted: Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val (question, answer) = questionToAnswer
+    var answerFieldValue by mutableStateOf(
+        TextFieldValue(text = answer.text, selection = TextRange(answer.text.length))
+    )
+    val isExpanded = remember { mutableStateOf(false) }
+
+    Card(modifier = modifier.padding(bottom = 10.dp)) {
+        Text(text = question.text, modifier = Modifier.padding(5.dp))
+
+        if (isExpanded.value) {
+            TextField(
+                value = answerFieldValue,
+                onValueChange = { newAnswer -> answerFieldValue = newAnswer },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp, vertical = 5.dp),
+                label = { Text(text = "Укажите тему и/или тип теста") } // Type the topic and/or type of a test
+            )
+
+            Button(onClick = {
+                saveAnswers(user, listOf(question to Answer(answerFieldValue.text)), false)
+            }) {
+                Text(text = stringResource(Res.string.save_button))
+            }
+        }
+    }
 }
--- compose/src/commonMain/kotlin/screens/test/PassingTestViewModel.kt
+++ compose/src/commonMain/kotlin/screens/test/PassingTestViewModel.kt
@@ -2,19 +2,60 @@ package screens.test
 
 import Answer
 import Question
+import TestAnswers
 import TestModel
+import User
 import androidx.compose.runtime.mutableStateOf
 import client.ClientRepository
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.essenty.lifecycle.coroutines.coroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class PassingTestViewModel(
     componentContext: ComponentContext,
     mainCoroutineContext: CoroutineContext,
     private val repository: ClientRepository,
-    test: TestModel,
+    val test: TestModel,
     val onFinished: () -> Unit,
 ) : ComponentContext by componentContext {
-    var answers =
+
+    private val scope = coroutineScope(mainCoroutineContext + SupervisorJob())
+
+    val questionsToAnswers =
         mutableStateOf(mutableListOf<Pair<Question, Answer>>())
+
+    init {
+        test.questions.forEach { question ->
+            questionsToAnswers.value.add(question to Answer(""))
+        }
+    }
+
+    fun saveAnswers(
+        user: User,
+        answers: List<Pair<Question, Answer>>,
+        isTestCompleted: Boolean,
+    ) {
+        // TODO: later save each answered question
+        if (isTestCompleted) {
+            val testAnswers = TestAnswers(test.id, user.email, answers)
+            scope.launch {
+                repository.saveAnswers(
+                    testAnswers = testAnswers,
+                    isTestCompleted = isTestCompleted
+                )
+            }
+            return
+        }
+        answers.forEach { questionToAnswer ->
+            val questionToAnswerIndex = questionsToAnswers.value
+                .indexOfFirst { it.first.id == questionToAnswer.first.id }
+            questionsToAnswers.value.set(
+                index = questionToAnswerIndex,
+                element = questionToAnswer
+            )
+        }
+    }
+
 }
