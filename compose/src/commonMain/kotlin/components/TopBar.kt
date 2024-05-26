c1ddaa0769ec5e91e977f1b0fd061fdb64786fef
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-26 16:12:51 +0300

    1) Add one more child to the TopBar: HorizontalDivider, put both in the Column;
    2) Change layout container from `Row` to `Box`,for the BackButton and the Title & Subtitle;
    
    3) Increase back button click zone;

--- compose/src/commonMain/kotlin/components/TopBar.kt
+++ compose/src/commonMain/kotlin/components/TopBar.kt
@@ -2,15 +2,19 @@ package components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TopBar(
@@ -19,16 +23,30 @@ fun TopBar(
     onBackButtonClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(modifier = modifier.background(color = MaterialTheme.colorScheme.surface)) {
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-            contentDescription = null,
-            modifier = Modifier.clickable(onClick = onBackButtonClick)
-        )
+    Column {
+        Box(
+            modifier = modifier
+                .background(color = MaterialTheme.colorScheme.surface)
+                .padding(horizontal = 10.dp),
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .clickable(onClick = onBackButtonClick)
+                    .padding(10.dp)
+            )
 
-        Column {
-            Title(text = title, modifier = Modifier.fillMaxWidth())
-            Subtitle(text = subtitle, modifier = Modifier.fillMaxWidth())
+            Column(modifier = Modifier.align(Alignment.Center)) {
+                Title(text = title, modifier = Modifier.fillMaxWidth())
+                Subtitle(text = subtitle, modifier = Modifier.fillMaxWidth())
+            }
         }
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 1.dp),
+            thickness = 1.dp,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
     }
 }
