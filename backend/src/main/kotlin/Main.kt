467ba96d3b8e559e851f52cb77c2fcd11500ffeb
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-26 17:18:01 +0300

    fix backend main AI usage example, after getAssessment() update

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -4,10 +4,10 @@ import datalayer.repository.Repository
 // TODO: When start implement Client (mobile app) - think about logic that we can share to Backend
 fun main() {
     val repository = Repository()
-    val assessments = mutableListOf<Float>()
+    val assessments = mutableListOf<Double>()
     val questionsToAnswers = repository.getAllQuestionsToAnswers()
     for ((question, answer) in questionsToAnswers) {
-        assessments.add(answer.getAssessment(question))
+        assessments.add(answer.getAssessment(question).second)
     }
     println("Assessments: $assessments")
 }
