da4527181f0545833e212ec214948723a4c6af59
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-29 10:52:12 +0300

    fixing backend module, with adding allow to use ContentNegotiation plugin, to serialize DTOs from server to JSON to send it via HTTP
    tested: server starts output correct responses

--- backend/src/main/kotlin/api/server/main.kt
+++ backend/src/main/kotlin/api/server/main.kt
@@ -6,11 +6,14 @@ import Repository
 import SqlDriverFactory
 import api.server.routes.configureRouting
 import createDatabase
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(
@@ -23,6 +26,12 @@ fun main() {
 
 fun Application.module() {
     install(CallLogging)
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
     val repository = Repository(createDatabase(SqlDriverFactory()))
     configureRouting(repository)
 }
