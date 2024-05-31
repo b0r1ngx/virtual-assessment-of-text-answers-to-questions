4b98b19511abf9a34d33cad6ebbffb4cceaaa913
Ivanov Kirill A <perkeboring@gmail.com>
2024-05-31 14:53:47 +0300

    change the way how a database file is stored, now its lay at backend module under /database folder (using JDBC driver, is not checking if tables are already exists, so need to add it to each entity of .sq (do it now) or change target of backend module from jvm to native (do it later, cos this changes are time consuming))

--- common/src/jvmMain/kotlin/SqlDriver.jvm.kt
+++ common/src/jvmMain/kotlin/SqlDriver.jvm.kt
@@ -4,7 +4,9 @@ import dev.boringx.Database
 
 actual class SqlDriverFactory {
     actual fun createDriver(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        val pathToDatabaseFile = "./backend/src/main/database/va.db"
+        val url = "jdbc:sqlite:$pathToDatabaseFile"
+        val driver: SqlDriver = JdbcSqliteDriver(url = url)
         Database.Schema.create(driver)
         return driver
     }
