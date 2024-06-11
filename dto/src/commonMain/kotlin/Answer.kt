f4d24998bd49bd8a024a2c17155dce0266b82f0b
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-11 14:46:05 +0300

    update DTO entity of Answer with avgMarAi of answer, that later provided when users wants to get it on ResultTestScreen

--- dto/src/commonMain/kotlin/Answer.kt
+++ dto/src/commonMain/kotlin/Answer.kt
@@ -3,5 +3,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Answer(
     val id: Long = -1,
-    val text: String
+    val text: String,
+    val avgMarkAi: Double = -1.0,
 )
