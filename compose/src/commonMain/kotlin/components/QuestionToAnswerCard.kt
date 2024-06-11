9e722aba3592d5f2d90b9e5fd75803c516720381
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-11 22:01:23 +0300

    1) modify QuestionToAnswerCard with shape = RoundedCornerBy16; 2) add modifier parameter;

--- compose/src/commonMain/kotlin/components/QuestionToAnswerCard.kt
+++ compose/src/commonMain/kotlin/components/QuestionToAnswerCard.kt
@@ -4,16 +4,26 @@ import Answer
 import Question
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import styles.RoundedCornerBy16
 
 @Composable
-fun QuestionToAnswerCard(question: Question, answer: Answer) {
-    Card {
+fun QuestionToAnswerCard(
+    question: Question,
+    answer: Answer,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier.padding(5.dp),
+        shape = RoundedCornerBy16,
+    ) {
         Column(modifier = Modifier.fillMaxWidth()) {
             Text(text = "Вопрос:", fontWeight = FontWeight.Bold)
             Text(text = question.text)
