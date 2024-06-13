a97715fff1451ea62e396f8468662ecdb5f7922b
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-14 02:47:15 +0300

    wants implement tests for routes, but get an idea, that may be we can use ClientApi, than test it with expected data, with that we test both Client & Server APIs

--- backend/src/test/kotlin/server/ApplicationTest.kt
+++ backend/src/test/kotlin/server/ApplicationTest.kt
@@ -4,19 +4,30 @@ import api.server.module
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
+    fun testRoutes() = testApplication {
         application {
             module()
         }
+        helloRouteTest()
+        userRouteTest()
+    }
+
+    suspend fun ApplicationTestBuilder.helloRouteTest() {
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello, world!", bodyAsText())
         }
     }
+
+    suspend fun ApplicationTestBuilder.userRouteTest() {
+
+    }
+
 }
