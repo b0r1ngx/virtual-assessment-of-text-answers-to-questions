5444846b23e64a9ebe1ed24ddabedc6c991b54df
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-13 14:00:31 +0300

    provide AssessTestScreen with information of virtual assessment overall mark for the test

--- compose/src/commonMain/kotlin/screens/test/AssessTestScreen.kt
+++ compose/src/commonMain/kotlin/screens/test/AssessTestScreen.kt
@@ -3,6 +3,7 @@ package screens.test
 import UserModel
 import UserViewModel
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,6 +15,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import components.QuestionToAnswerToAvgMarkAiCard
@@ -54,6 +56,13 @@ private fun BottomBar(
     testViewModel: AssessTestViewModel
 ) {
     Column(modifier = Modifier.padding(top = 10.dp, start = 10.dp, end = 10.dp)) {
+        Row(modifier = Modifier.padding(bottom = 5.dp)) {
+            Text(text = "Предварительная отметка ассистента: ")
+            Text(
+                text = testViewModel.testAnswers.avgMarkAi.round(1).toString(),
+                fontWeight = FontWeight.Bold,
+            )
+        }
         TextField(
             value = testViewModel.assessment,
             onValueChange = { inputAssess ->
