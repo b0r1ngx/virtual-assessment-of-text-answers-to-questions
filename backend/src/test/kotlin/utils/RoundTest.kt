04355cdd87907abb81183769607280abd3e7b9e9
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-15 23:40:21 +0300

    fix test names to camelCase

--- backend/src/test/kotlin/utils/RoundTest.kt
+++ backend/src/test/kotlin/utils/RoundTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 class RoundTest {
 
     @Test
-    fun round_down() {
+    fun roundDown() {
         val notFormattedNumber = 7.1477
         val expected = 7.1
         val actual = notFormattedNumber.round(1)
@@ -14,7 +14,7 @@ class RoundTest {
     }
 
     @Test
-    fun round_up() {
+    fun roundUp() {
         val notFormattedNumber = 7.1512
         val expected = 7.2
         val actual = notFormattedNumber.round(1)
