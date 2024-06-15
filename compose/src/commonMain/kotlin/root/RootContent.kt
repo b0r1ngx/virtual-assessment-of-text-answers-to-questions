07360664fdaac8287257329e8f7e3d9ebb436ec4
Ivanov Kirill A <perkeboring@gmail.com>
2024-06-16 00:53:28 +0300

    1) providing snackbarHostState to the Children App Content at the RootContent;
    2) Using scaffold instead of surface, to use snackbarHost with it;

--- compose/src/commonMain/kotlin/root/RootContent.kt
+++ compose/src/commonMain/kotlin/root/RootContent.kt
@@ -1,11 +1,18 @@
 package root
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
@@ -21,45 +28,57 @@ import screens.test.ResultTestScreen
 import screens.tests.TestsScreen
 import theme.AppTheme
 
-// TODO: Use Scaffold
+private val LocalSnackbarHostState = compositionLocalOf<SnackbarHostState> {
+    error("No Snackbar Host State provided")
+}
+
 @Composable
 internal fun RootContent(
     component: RootComponent,
     modifier: Modifier = Modifier,
 ) {
-    AppTheme {
-        Surface(modifier = modifier.fillMaxSize().windowInsetsPadding(WindowInsets.systemBars)) {
-            Children(
-                stack = component.navigationStack,
-                modifier = Modifier.fillMaxSize(),
-                animation = stackAnimation(fade() + scale())
-            ) {
-                when (val instance = it.instance) {
-                    is RootComponent.Child.Auth -> AuthScreen(authViewModel = instance.component)
+    val snackbarHostState = remember { component.snackbarHostState }
 
-                    is RootComponent.Child.Tests -> TestsScreen(
-                        userViewModel = component.userViewModel,
-                        testsViewModel = instance.component,
-                    )
+    CompositionLocalProvider(
+        LocalSnackbarHostState provides snackbarHostState,
+    ) {
+        AppTheme {
+            Scaffold(
+                modifier = modifier.fillMaxSize().windowInsetsPadding(WindowInsets.systemBars),
+                snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+            ) { paddingValues: PaddingValues ->
+                Children(
+                    stack = component.navigationStack,
+                    modifier = Modifier.consumeWindowInsets(paddingValues).fillMaxSize(),
+                    animation = stackAnimation(fade() + scale())
+                ) {
+                    when (val instance = it.instance) {
+                        is RootComponent.Child.Auth -> AuthScreen(authViewModel = instance.component)
 
-                    is RootComponent.Child.EditingTest -> EditingTestScreen(
-                        userViewModel = component.userViewModel,
-                        testViewModel = instance.component,
-                    )
+                        is RootComponent.Child.Tests -> TestsScreen(
+                            userViewModel = component.userViewModel,
+                            testsViewModel = instance.component,
+                        )
 
-                    is RootComponent.Child.PassingTest -> PassingTestScreen(
-                        userViewModel = component.userViewModel,
-                        testViewModel = instance.component,
-                    )
+                        is RootComponent.Child.EditingTest -> EditingTestScreen(
+                            userViewModel = component.userViewModel,
+                            testViewModel = instance.component,
+                        )
 
-                    is RootComponent.Child.AnswersTest -> AnswersTestScreen(testViewModel = instance.component)
+                        is RootComponent.Child.PassingTest -> PassingTestScreen(
+                            userViewModel = component.userViewModel,
+                            testViewModel = instance.component,
+                        )
 
-                    is RootComponent.Child.AssessTest -> AssessTestScreen(
-                        userViewModel = component.userViewModel,
-                        testViewModel = instance.component
-                    )
+                        is RootComponent.Child.AnswersTest -> AnswersTestScreen(testViewModel = instance.component)
 
-                    is RootComponent.Child.ResultTest -> ResultTestScreen(testViewModel = instance.component)
+                        is RootComponent.Child.AssessTest -> AssessTestScreen(
+                            userViewModel = component.userViewModel,
+                            testViewModel = instance.component
+                        )
+
+                        is RootComponent.Child.ResultTest -> ResultTestScreen(testViewModel = instance.component)
+                    }
                 }
             }
         }
